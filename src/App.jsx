2c12bed39726337876b496d9e59a25d135613cc9
Gubrux <gabrielcentur@gmail.com>
2024-03-12 16:26:30 -0300

    add the validation in email, pass, and name

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,81 @@
-import react, { useState } from  'react';
-    
+import React, { useState } from  'react';
     
 const UserForm = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+    const [title, setTitle] = useState("")
+    const [titleError, setTitleError] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
     
     const createUser = (e) => {
         e.preventDefault();
         const newUser = { username, email, password };
-        console.log("Welcome", newUser.username);
         setHasBeenSubmitted( true );
     };
+
+    const handleTitle = (e) => {
+      setUsername(e.target.value); // Cambia setTitle a setUsername
+      if(e.target.value.length < 1) {
+        setTitleError("Title is required!");
+      } else if(e.target.value.length < 3) {
+        setTitleError("Title must be 3 characters or longer!");
+      } else {
+        setTitleError("");
+      }
+    }
+
+  const handleEmail = (e) => {
+      setEmail(e.target.value);
+      if(e.target.value.length < 1) {
+          setEmailError("Email is required!");
+      } else if(!e.target.value.includes("@") || !e.target.value.includes(".com") || e.target.value.length < 5 || e.target.value.includes(" ")) {
+          setEmailError("Email must be valid!");
+      } else {
+          setEmailError("");
+      }
+  }
+
+  const handlePassword = (e) => {
+      setPassword(e.target.value);
+      if(e.target.value.length < 1) {
+          setPasswordError("Password is required!");
+      } else if(e.target.value.length < 8) {
+          setPasswordError("Password must be 8 characters or longer!");
+      } else {
+          setPasswordError("");
+      }
+  }
+
     return (
         <form onSubmit={ createUser }>
           {
           hasBeenSubmitted ? 
-          <h3>{`Thank you, ${username} for submitting the form!`}</h3> :
-          <h3>Welcome, please submit the form.</h3> 
+          <h3>{`Gracias ${username}, por regalarnos tus datos!!`}</h3> :
+          <h3>Bienvenido, complete el formulario por favor</h3> 
             }
             <div>
-                <label>Username: </label> 
-                <input type="text" onChange={ (e) => setUsername(e.target.value) } />
+              <label>Username: </label> 
+              <input type="text" onChange={ handleTitle } />
+              {
+                titleError ?
+                <p style={{color:'red'}}>{ titleError }</p> :
+                ''
+              }
             </div>
             <div>
-                <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+              <label>Email Address: </label> 
+              <input type="text" onChange={ handleEmail } />
+              { emailError && <p style={{color:'red'}}>{ emailError }</p> }
             </div>
             <div>
-                <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+              <label>Password: </label>
+              <input type="password" onChange={ handlePassword } />
+              { passwordError && <p style={{color:'red'}}>{ passwordError }</p> }
             </div>
-            <input type="submit" value="Create User"/>
+            <input type="submit" value="submit"/>
         </form>
     );
 };
